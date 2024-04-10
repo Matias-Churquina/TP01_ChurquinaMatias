263cf5769420759144692e8e3782136eccadb400
Matias <Matias@DESKTOP-9B903QQ>
2024-04-10 19:51:25 -0300

    Resolucion hasta punto 3.

---
+++ tp1_ChurquinaMatias/src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -0,0 +1,22 @@
+package ar.edu.unju.fi.ejercicio2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String pais = "Italia";
+		byte edad = 27;
+		float altura = 40.70f;
+		double precio = 345.654934;
+		long celular = 388547373;
+		double cos = 0.5;
+		double coseno = Math.cos(cos);
+		
+		System.out.println("País: "+ pais );
+		System.out.println("Edad: "+ edad );
+		System.out.println("Altura: "+ altura );
+		System.out.println("Precio: "+ precio );
+		System.out.println("Celular: "+ celular );
+		System.out.println("Coseno de " + cos + " : "+ coseno );
+	}
+
+}
---
+++ tp1_ChurquinaMatias/src/ar/edu/unju/fi/ejercicio3/Main.java
@@ -0,0 +1,20 @@
+package ar.edu.unju.fi.ejercicio3;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Ingrese un número entero:");
+		int num = sc.nextInt();
+		if (num%2 == 0) {
+			System.out.println("Número ingresado par. ");
+			System.out.print("Resultado: " + (num * 3));
+		} else {
+			System.out.println("Número ingresado impar. ");
+			System.out.print("Resultado: " + (num * 2));
+		}
+	}
+
+}
