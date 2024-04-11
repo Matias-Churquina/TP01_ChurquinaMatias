db0f47bb71c5803ceffa2a267e3d05981b44e5fe
Matias <Matias@DESKTOP-9B903QQ>
2024-04-10 21:16:14 -0300

    Punto 4: Factorial

---
+++ tp1_ChurquinaMatias/src/ar/edu/unju/fi/ejercicio4/Main.java
@@ -0,0 +1,21 @@
+package ar.edu.unju.fi.ejercicio4;
+
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Ingrese un número entre 0 y 10: ");
+		int num = sc.nextInt();
+		int aux = num;
+		int fac = 1;
+		while (aux > 0) {
+			if ( aux != 0 ) {
+				fac = fac * aux;
+				aux --;
+			} 
+		}
+		System.out.print("El Factorial de " + num + " es: " + fac );
+	}
+
+}
