51f97777d6bc9c7fc917f89e5f99b5b441db3d43
Matias <Matias@DESKTOP-9B903QQ>
2024-04-17 22:52:17 -0300

    Punto 13.

---
+++ tp1_ChurquinaMatias/src/ar/edu/unju/fi/ejercicio13/Main.java
@@ -0,0 +1,29 @@
+package ar.edu.unju.fi.ejercicio13;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		List<Integer> arreglo = new ArrayList<>();
+		int i = 1;
+		do {
+			System.out.print("Ingrese un número entero: ");
+			Integer ob = sc.nextInt();
+			arreglo.add(ob);
+			i++;
+		}while(i<9);
+		System.out.println("    --- ARRAY ---");
+		i = -1;
+		for (Integer in:arreglo) {
+			i += 1;
+			System.out.println(" Índice= " + i  + 
+					           " Valor= " + in );
+		}
+		sc.close();
+	}
+}
